ae6a5414f156be26c6da20ec08d3e0d2fdb76157
SylviaKoto <sylviakoto@gmail.com>
2025-04-19 14:00:30 +0000

    update

--- SeleniumScratch/src/test/java/SylviaAcademy/factory/DriverFactory.java
+++ SeleniumScratch/src/test/java/SylviaAcademy/factory/DriverFactory.java
@@ -5,23 +5,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
-    
+
     public static WebDriver createDriver(String browser) {
         WebDriver driver;
+
+        String headless = System.getProperty("headless");
+        boolean isHeadless = "true".equalsIgnoreCase(headless);
 
         switch (browser.toLowerCase()) {
             case "chrome":
                 WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                if (isHeadless) {
+                    chromeOptions.addArguments("--headless", "--disable-gpu", "--window-size=1920,1080");
+                }
+                driver = new ChromeDriver(chromeOptions);
                 break;
 
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                if (isHeadless) {
+                    firefoxOptions.addArguments("--headless");
+                }
+                driver = new FirefoxDriver(firefoxOptions);
                 break;
 
             case "edge":
@@ -37,7 +49,7 @@ public class DriverFactory {
                 throw new IllegalArgumentException("Navigateur non supporté : " + browser);
         }
 
-        // Stocker le driver dans ThreadLocal
+        // Store the driver in ThreadLocal
         DriverManager.setDriver(driver);
         return driver;
     }
